d9adfa20fa076855307cb3d39742d45fe2e2378d
guym <guym@gigaspaces.com>
2014-02-05 10:17:11 +0200

    first commit

---
+++ cloudify-widget-api/src/main/java/cloudify/widget/api/clouds/CloudCreateServerOptions.java
@@ -0,0 +1,10 @@
+package cloudify.widget.api.clouds;
+
+/**
+ * 
+ * @author evgenyf
+ * Date: 10/7/13
+ */
+public interface CloudCreateServerOptions {
+
+}
---
+++ cloudify-widget-api/src/main/java/cloudify/widget/api/clouds/CloudServerCreated.java
@@ -0,0 +1,11 @@
+package cloudify.widget.api.clouds;
+
+/**
+ * 
+ * @author evgenyf
+ * Date: 10/7/13
+ */
+public interface CloudServerCreated {
+
+	String getId();
+}
