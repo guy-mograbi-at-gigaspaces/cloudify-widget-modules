0a74e4c61b67963c25a3f0f01b5cecaa1940f749
eliranmal <eliranmal@gmail.com>
2014-03-04 16:42:40 +0200

    pool manager - adding equals and hashcode

--- cloudify-widget-pool-manager/src/main/java/cloudify/widget/pool/manager/dto/ManagerSettings.java
+++ cloudify-widget-pool-manager/src/main/java/cloudify/widget/pool/manager/dto/ManagerSettings.java
@@ -4,10 +4,10 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
-* User: eliranm
-* Date: 2/27/14
-* Time: 3:21 PM
-*/
+ * User: eliranm
+ * Date: 2/27/14
+ * Time: 3:21 PM
+ */
 public class ManagerSettings {
 
     private List<PoolSettings> pools = new LinkedList<PoolSettings>();
@@ -18,5 +18,19 @@ public class ManagerSettings {
 
     public void setPools(List<PoolSettings> pools) {
         this.pools = pools;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ManagerSettings that = (ManagerSettings) o;
+        if (!pools.equals(that.pools)) return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return pools.hashCode();
     }
 }
