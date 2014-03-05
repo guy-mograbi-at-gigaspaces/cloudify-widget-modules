fcd0d74993d369da9ca046aa0a56c5139b6852ee
eliranmal <eliranmal@gmail.com>
2014-03-05 17:19:41 +0200

    pool manager - adding pools wrapper object

--- cloudify-widget-pool-manager/src/main/java/cloudify/widget/pool/manager/dto/ManagerSettings.java
+++ cloudify-widget-pool-manager/src/main/java/cloudify/widget/pool/manager/dto/ManagerSettings.java
@@ -1,7 +1,5 @@
 package cloudify.widget.pool.manager.dto;
 
-import java.util.*;
-
 /**
  * User: eliranm
  * Date: 2/27/14
@@ -9,76 +7,13 @@ import java.util.*;
  */
 public class ManagerSettings {
 
-/*
-    public static class Pools {
+    private PoolsSettingsList pools = new PoolsSettingsList();
 
-        private final List<PoolSettings> list = new LinkedList<PoolSettings>();
-
-        public List<PoolSettings> getList() {
-            return list;
-        }
-
-        public boolean isEmpty() {
-            return list.isEmpty();
-        }
-
-        public boolean containsAll(Collection<?> c) {
-            return list.containsAll(c);
-        }
-
-        public ListIterator<PoolSettings> listIterator() {
-            return list.listIterator();
-        }
-
-        public List<PoolSettings> subList(int fromIndex, int toIndex) {
-            return list.subList(fromIndex, toIndex);
-        }
-
-        public boolean equals(Object o) {
-            return list.equals(o);
-        }
-
-        public int hashCode() {
-            return list.hashCode();
-        }
-
-        public Iterator<PoolSettings> iterator() {
-            return list.iterator();
-        }
-
-        public boolean contains(Object o) {
-            return list.contains(o);
-        }
-
-        public int size() {
-            return list.size();
-        }
-
-        public PoolSettings get(int index) {
-            return list.get(index);
-        }
-
-        public int indexOf(Object o) {
-            return list.indexOf(o);
-        }
-
-        public int lastIndexOf(Object o) {
-            return list.lastIndexOf(o);
-        }
-
-        public ListIterator<PoolSettings> listIterator(int index) {
-            return list.listIterator(index);
-        }
-    }
-*/
-
-    private List<PoolSettings> pools = new LinkedList<PoolSettings>();
-
-    public List<PoolSettings> getPools() {
+    public PoolsSettingsList getPools() {
         return pools;
     }
 
-    public void setPools(List<PoolSettings> pools) {
+    public void setPools(PoolsSettingsList pools) {
         this.pools = pools;
     }
 
