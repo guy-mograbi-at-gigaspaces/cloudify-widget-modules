38d9e539e7bf1a74b785280063196fd0a9b6e6d5
Evgeny Fisher <evgenyf@gigaspaces.com>
2014-03-06 10:07:31 +0200

    Return Objects within responseEntity without parsing to JSON string

--- cloudify-widget-pool-manager-website/src/main/java/cloudify/widget/website/controller/IndexController.java
+++ cloudify-widget-pool-manager-website/src/main/java/cloudify/widget/website/controller/IndexController.java
@@ -4,7 +4,6 @@ import cloudify.widget.website.dao.IAccountDao;
 import cloudify.widget.website.dao.IPoolDao;
 import cloudify.widget.website.models.AccountModel;
 import cloudify.widget.website.models.PoolConfigurationModel;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,14 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 import java.util.List;
 
 
@@ -35,37 +29,27 @@ public class IndexController {
     @Autowired
     private IPoolDao poolDao;
 
-    private ObjectMapper mapper = new ObjectMapper();
-
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
     public String showIndex(  @ModelAttribute("account") AccountModel accountModel ) {
         logger.info("account model id is : " + accountModel.id );
-        throw new RuntimeException("this is me!!!!");
-//        return "hello world!";
+        //throw new RuntimeException("this is me!!!!");
+        return "hello world!";
     }
 
     @RequestMapping(value="/account/{uuid}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ResponseEntity<String> getAccount( @PathVariable("uuid") String uuid ) {
+    public ResponseEntity<AccountModel> getAccount( @PathVariable("uuid") String uuid ) {
 
         AccountModel accountModel = accountDao.readAccountByUuid( uuid );
-        ResponseEntity<String> retValue;
+        ResponseEntity<AccountModel> retValue;
 
         if( accountModel == null ) {
-            retValue = new ResponseEntity<String>( HttpStatus.NOT_FOUND );
+            retValue = new ResponseEntity<AccountModel>( HttpStatus.NOT_FOUND );
         }
         else{
-            try {
-                String objectStr = mapper.writeValueAsString(accountModel);
-                retValue = new ResponseEntity<String>( objectStr, HttpStatus.OK );
-            }
-            catch( IOException e ) {
-                if( logger.isErrorEnabled() ){
-                    logger.error( "Unable to parse Account with uuid [" + uuid + "] to Json", e );
-                }
-                retValue = new ResponseEntity<String>( HttpStatus.NOT_FOUND );
-            }
+
+            retValue = new ResponseEntity<AccountModel>( accountModel, HttpStatus.OK );
         }
 
         return retValue;
@@ -85,30 +69,20 @@ public class IndexController {
 
     @RequestMapping(value="/pool/{poolId}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ResponseEntity<String> getPoolConfiguration( @ModelAttribute("account") AccountModel accountModel, @PathVariable("poolId") Long poolId ) {
+    public ResponseEntity<PoolConfigurationModel> getPoolConfiguration( @ModelAttribute("account") AccountModel accountModel, @PathVariable("poolId") Long poolId ) {
 
         PoolConfigurationModel poolConfigurationModel = poolDao.readPoolByAccountId( poolId, accountModel);
-        ResponseEntity<String> retValue;
+        ResponseEntity<PoolConfigurationModel> retValue;
 
         if( poolConfigurationModel == null ) {
-            retValue = new ResponseEntity<String>( HttpStatus.NOT_FOUND );
+            retValue = new ResponseEntity<PoolConfigurationModel>( HttpStatus.NOT_FOUND );
         }
         else{
-            try {
-                String objectStr = mapper.writeValueAsString(poolConfigurationModel);
-                retValue = new ResponseEntity<String>( objectStr, HttpStatus.OK );
-            }
-            catch( IOException e ) {
-                if( logger.isErrorEnabled() ){
-                    logger.error( "Unable to parse Pool Configuration with account id [" + accountModel.id + "] to Json", e );
-                }
-                retValue = new ResponseEntity<String>( HttpStatus.NOT_FOUND );
-            }
+            retValue = new ResponseEntity<PoolConfigurationModel>( poolConfigurationModel, HttpStatus.OK );
         }
 
         return retValue;
     }
-
 
     @ModelAttribute("account")
     public AccountModel getUser(HttpServletRequest request)
