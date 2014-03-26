e667f75731d46d7c7d1821e327db604620777804
guym <guym@gigaspaces.com>
2014-03-27 00:03:47 +0200

    fixing gsat installation.

--- cloudify-widget-pool-manager-website/build/start.sh
+++ cloudify-widget-pool-manager-website/build/start.sh
@@ -1,12 +1,12 @@
-PROPERTIES_FILE=/opt/pool-manager-website/manager.properties
+source /etc/sysconfig/widget-pool-manager
 
-if [ ! -f $PROPERTIES_FILE ];then
-    echo "you must define $PROPERTIES_FILE"
+if [ ! -f $PROPERTIES_RESOURCE ];then
+    echo "you must define $PROPERTIES_RESOURCE"
     exit 1
 fi
 
 VM_ARGS="-Dspring.profiles.active=manager-app"
-VM_ARGS="$VM_ARGS  -DmanagerPropertiesLocation=file:$PROPERTIES_FILE"
+VM_ARGS="$VM_ARGS  -DmanagerPropertiesLocation=$PROPERTIES_RESOURCE"
 
 
 java $VM_ARGS -jar ../website-1.0.0.jar
