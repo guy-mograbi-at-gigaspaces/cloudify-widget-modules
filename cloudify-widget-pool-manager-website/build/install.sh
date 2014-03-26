30e8a122f0f5cd625e7f7e31a148be384867eaa7
guym <guym@gigaspaces.com>
2014-03-26 23:12:39 +0200

    fixing gsat installation.

--- cloudify-widget-pool-manager-website/build/install.sh
+++ cloudify-widget-pool-manager-website/build/install.sh
@@ -49,6 +49,7 @@ download_pool_manager(){
     mkdir -p $INSTALL_LOCATION
     tar -xf $TAR_FILENAME -C $INSTALL_LOCATION
     mv $INSTALL_LOCATION/target* $INSTALL_LOCATION/website-1.0.0.jar
+    chmod +x $INSTALL_LOCATION/**/*.sh
 }
 
 
