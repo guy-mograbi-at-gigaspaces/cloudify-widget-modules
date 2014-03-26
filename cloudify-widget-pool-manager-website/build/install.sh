f21b747e8dc86bc00db40170266707778a24f7f0
guym <guym@gigaspaces.com>
2014-03-26 19:13:38 +0200

    adding "start.sh" and referencing it in service.

--- cloudify-widget-pool-manager-website/build/install.sh
+++ cloudify-widget-pool-manager-website/build/install.sh
@@ -1,44 +1,3 @@
-read_sysconfig(){
-
-
-
-    if [ ! -f $SYSCONFIG_FILE ]; then
-        echo "sysconfig file does not exists"
-        touch /etc/sysconfig/cwpm
-    fi
-    if [ -z $INSTALL_LOCATION ]; then
-        echo "install location is undefined, setting default"
-        echo "INSTALL_LOCATION=/opt/cwpm/cwpm.jar" >> $SYSCONFIG_FILE
-    fi
-}
-
-download_pool_manager(){
-    URL=http://get.gsdev.info/cloudify-widget-pool-manager-website/1.0.0/cloudify-widget-pool-manager-website-1.0.0.tar
-
-    TAR_FILENAME=cloudify-widget-pool-manager-website-1.0.0.tar
-    wget --no-check-certificate "$URL" -O $TAR_FILENAME
-
-    OPT_LOCATION=/opt/cloudify-widget-pool-manager
-    mkdir -p $OPT_LOCATION
-    tar -xf $TAR_FILENAME -C $OPT_LOCATION
-    mv $OPT_LOCATION/target* $OPT_LOCATION/website-1.0.0.jar
-}
-
-install_service(){
-    SERVICE_NAME=widget-pool
-    INITD_LOCATION=/etc/init.d/$SERVICE_NAME
-    SERVICE_FILE=$OPT_LOCATION/build/service.sh
-
-    cp $SERVICE_FILE $INITD_LOCATION
-    chmod +x $INITD_LOCATION
-}
-
-
-
-
-
-
-
 main(){
 
     SYSCONFIG_FILE=/etc/sysconfig/widget-pool-manager
@@ -50,37 +9,34 @@ main(){
     CURRENT_DIRECTORY=`pwd`
     cd "$(dirname "$0")"
 
-    if [ ! -f gsui_functions.sh ]; then
+    if [ ! -f /opt/gsat/gsui_functions.sh ]; then
 
+        mkdir /opt/gsat
         echo "download gsui_functions.sh"
-        wget --no-cache --no-check-certificate -O gsat.tar http://get.gsdev.info/gsat/1.0.0/gsat-1.0.0.tar
-
-        tar -xvf gsat.tar
-
+        wget --no-cache --no-check-certificate -O /opt/gsat/gsat.tar http://get.gsdev.info/gsat/1.0.0/gsat-1.0.0.tar
+        tar -xvf /opt/gsat/gsat.tar
 
     fi
 
     echo "loading gsat functions"
-    dos2unix *.sh
-    chmod +x *.sh
-    source ./gsui_functions.sh
+    dos2unix *.sh /opt/gsat/*.sh
+    chmod +x *.sh /opt/gsat/*.sh
+    source /opt/gsat/gsui_functions.sh
     echo "gsat functions loaded"
 
     echo "reading sysconfig"
-    read_sysconfig $*
+    SYSCONFIG_FILE=pool-manager read_sysconfig
+    check_exists INSTALL_LOCATION
 
 
     install_java
 
     install_nginx
+    source nginx.conf > /etc/nginx/sites-enabled/widget-pool-manager.conf
 
     install_mysql
 
-    echo "installing the JAR file"
-    download_pool_manager $*
-
-    echo "installing service script under widget-pool"
-    install_service $*
+    upgrade
 
     cd $CURRENT_DIRECTORY
 
