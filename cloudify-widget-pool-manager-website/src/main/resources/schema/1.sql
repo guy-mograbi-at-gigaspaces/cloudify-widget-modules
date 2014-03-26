a0f8bdde2feef22be4f1e38619b9fd50c086ac5f
guym <guym@gigaspaces.com>
2014-03-27 01:30:10 +0200

    fixing gsat installation.

--- cloudify-widget-pool-manager-website/src/main/resources/schema/1.sql
+++ cloudify-widget-pool-manager-website/src/main/resources/schema/1.sql
@@ -1,5 +1,4 @@
 CREATE TABLE pool_configuration ( id BIGINT NOT NULL AUTO_INCREMENT, account_id BIGINT, pool_setting TEXT, PRIMARY KEY (id) );
-CREATE TABLE `patchlevel` ( `version` bigint(20) NOT NULL DEFAULT '0',  PRIMARY KEY (`version`));
 CREATE TABLE account ( id BIGINT NOT NULL AUTO_INCREMENT, uuid VARCHAR(255) DEFAULT NULL, PRIMARY KEY (id) );
 
 
