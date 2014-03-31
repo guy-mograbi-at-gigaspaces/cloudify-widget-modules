b46b7ea364a28a0d88abf58cb5ca95b729505ae1
guym <guym@gigaspaces.com>
2014-03-31 15:43:18 +0200

    fixing start script.

--- cloudify-widget-pool-manager-website/build/start.sh
+++ cloudify-widget-pool-manager-website/build/start.sh
@@ -1,10 +1,5 @@
 source /etc/sysconfig/widget-pool-manager
 
-if [ -z PROPERTIES_RESOURCE ] || [ "$PROPERTIES_RESOURCE" = "" ];then
-    echo "you must define $PROPERTIES_RESOURCE"
-    exit 1
-fi
-
 VM_ARGS="-Dspring.profiles.active=manager-app"
 VM_ARGS="$VM_ARGS -DwebsiteMeContext=file://$INSTALL_LOCATION/build/me-context.xml"
 
