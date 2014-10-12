6aa22e8377459f83dff93822c205396c5af8d2aa
guym <guym@gigaspaces.com>
2014-10-12 18:36:16 +0300

    fix(run): implement check if management exists for ec2.

--- cloudify-widget-api/src/main/java/cloudify/widget/api/clouds/CloudProvider.java
+++ cloudify-widget-api/src/main/java/cloudify/widget/api/clouds/CloudProvider.java
@@ -1,6 +1,10 @@
 package cloudify.widget.api.clouds;
 
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * User: guym
@@ -9,19 +13,23 @@ package cloudify.widget.api.clouds;
  */
 public enum CloudProvider {
     HP("hpcloud-compute"),
-    AWS_EC2("aws_ec2"),
+    AWS_EC2("aws-ec2","aws_ec2"),
     SOFTLAYER("softlayer"),
     NA("na");
 
     public String label;
+    public List<String> labels;
 
-    private CloudProvider( String label ) {
-        this.label = label;
+    private CloudProvider( String ... labels ) {
+
+        this.labels = Arrays.asList(labels);
+        this.label = labels[0];
+
     }
 
     public static CloudProvider findByLabel(String cloudProvider) {
         for (CloudProvider provider : values()) {
-            if ( provider.label.equals(cloudProvider)){
+            if ( provider.labels.contains(cloudProvider)){
                 return provider;
             }
         }
