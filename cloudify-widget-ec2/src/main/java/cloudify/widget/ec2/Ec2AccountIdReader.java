e01175aa0162ee8a8a10b676fed1b1eba49852c0
guym <guym@gigaspaces.com>
2014-11-24 18:39:47 +0200

    fix account id algorithm. use: https://forums.aws.amazon.com/message.jspa?messageID=252875

--- cloudify-widget-ec2/src/main/java/cloudify/widget/ec2/Ec2AccountIdReader.java
+++ cloudify-widget-ec2/src/main/java/cloudify/widget/ec2/Ec2AccountIdReader.java
@@ -17,10 +17,14 @@ import com.amazonaws.services.identitymanagement.model.GetUserResult;
  */
 public class Ec2AccountIdReader {
 
+    // this is the recommended method of getting the accountId
+    // see: https://forums.aws.amazon.com/message.jspa?messageID=252875
     public String getAccountId( String apiKey, String apiSecretKey ){
         final AWSCredentials credentials = new BasicAWSCredentials(apiKey, apiSecretKey);
         AmazonIdentityManagementClient client = new AmazonIdentityManagementClient(credentials);
         GetUserResult user = client.getUser();
-        return user.getUser().getUserId();
+        String arn = user.getUser().getArn();
+        String accountId = arn.split("::")[1].split(":")[0];
+        return accountId;
     }
 }
